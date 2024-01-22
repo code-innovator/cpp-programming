7e0bd0ae00432e89bf978800bf5a90b98cb7ae63
code-innovator <146607353+code-innovator@users.noreply.github.com>
2024-01-22 23:14:57 +0530

    Code.cpp

--- Code - 023/Code.cpp
+++ Code - 023/Code.cpp
@@ -1,42 +1,42 @@
 //Negate all data items of class with - operator
-#include<iostream.h>
-#includes<conio.h>
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
 class space
 {
-        private:
-        int x,y,z;
-        public:
-                space(int a,int b,int c)
-                {
-                x=a;
-                y=b;
-                z=c;
-                }
-                void display(void);
-                void operator-(); //overloaded unary-
-                };
-                void space::display()
-                {
-                cout<<x<<"\t"<<y<<"\t"<<z;
-                }
-                void space::operator-(void)
-                {
-                x=-x;
-                y=-y;
-                z=-z;
-                }
-                void main()
-                {
-                clrscr();
-                int l,m,n;
-                space S;
-                cout<<"\n Enter three numbers: \n";
-                cin>>l>>m>>n;
-                S=space(l,m,n);
-                cout<<"S:";
-                S.display();
-                - S;
-                cout<<"-S:";
-                S.display();
-                getch();
-                }
+	private:
+	int x,y,z;
+	public:
+		space(int a,int b,int c)
+		{
+		x=a;
+		y=b;
+		z=c;
+		}
+		void display(void);
+		void operator-(); //overloaded unary-
+		};
+		void space::display()
+		{
+		cout<<x<<"\t"<<y<<"\t"<<z;
+		}
+		void space::operator-(void)
+		{
+		x=-x;
+		y=-y;
+		z=-z;
+		}
+		int main()
+		{
+		system("clear");
+		int l,m,n;
+		space S(l,m,n);
+		cout<<"\n Enter three numbers: \n";
+		cin>>l>>m>>n;
+		cout<<"\n S:";
+		S.display();
+		-S;
+		cout<<"\n-S:";
+		S.display();
+		getchar();
+		}
